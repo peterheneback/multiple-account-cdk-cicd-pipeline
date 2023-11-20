a6d853b6ce90523b493cba8e2a0e6e808146368e
Peter Heneback <heneback@amazon.co.uk>
2023-11-20 16:22:04 +0000

    Updated graphql.handler to simplest possible

--- api/src/graphql.ts
+++ api/src/graphql.ts
@@ -1,31 +1,38 @@
-import { ApolloServer, gql } from 'apollo-server-lambda';
-import { ApolloServerPluginLandingPageGraphQLPlayground } from 'apollo-server-core';
+// import { ApolloServer, gql } from 'apollo-server-lambda';
+// import { ApolloServerPluginLandingPageGraphQLPlayground } from 'apollo-server-core';
 
 import { typeDefs } from './typeDefs';
 import { resolvers } from './resolvers';
 import { db } from './db-connection';
 
-const createConnection = async () => {
-  await db();
+import { Handler } from 'aws-lambda';
+
+// const createConnection = async () => {
+//   await db();
+// };
+
+// createConnection();
+
+// const server = new ApolloServer({
+//   typeDefs,
+//   resolvers,
+//   context: ({ context }) => {
+//     context.callbackWaitsForEmptyEventLoop = false;
+//   },
+//   introspection: true,
+//   plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
+// });
+
+// export const handler = server.createHandler({
+//   expressGetMiddlewareOptions: {
+//     cors: {
+//       origin: '*',
+//       credentials: true,
+//     },
+//   },
+// });
+
+export const handler: Handler = async (event, context) => {
+  console.log('EVENT: \n' + JSON.stringify(event, null, 2));
+  return context.logStreamName;
 };
-
-createConnection();
-
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: ({ context }) => {
-    context.callbackWaitsForEmptyEventLoop = false;
-  },
-  introspection: true,
-  plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
-});
-
-export const handler = server.createHandler({
-  expressGetMiddlewareOptions: {
-    cors: {
-      origin: '*',
-      credentials: true,
-    },
-  },
-});
